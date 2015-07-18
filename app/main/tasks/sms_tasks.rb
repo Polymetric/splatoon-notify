cfd53e916a63ca6d295ecff9ccce301bd6500ac6
Polymetric <austin.is.paxton@gmail.com>
2015-07-18 17:38:02 -0500

    refix sms

--- app/main/tasks/sms_tasks.rb
+++ app/main/tasks/sms_tasks.rb
@@ -5,7 +5,7 @@ class SmsTasks < Volt::Task
   end
 
   def send_sms
-    store._players.each do |player|
+    store._players.each.then do |player|
       Easy.deliver(player._phone, player._carrier, "Splatoon/Notify!\nTurf War:\n#{MapTasks.printmap(0).sync}\n#{MapTasks.printmap(1).sync}\nRanked:\n#{MapTasks.printmap(2).sync}\n#{MapTasks.printmap(3).sync}", :limit => 512 )
     end
   end
